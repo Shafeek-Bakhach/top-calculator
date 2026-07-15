87fd980aff030b9d56f0d47be9a0fabfe52e10fe
Shafeek <shafeek.bakhach@gmail.com>
2026-07-15 07:04:22 -0700

    Creates basic HTML/CSS layout for buttons

--- script.js
+++ script.js
@@ -2,3 +2,19 @@ let add = (x1, x2) => x1 + x2;
 let subtract = (x1, x2) => x1 - x2;
 let multiply = (x1, x2) => x1 * x2;
 let divide = (x1, x2) => x1 / x2;
+
+let num1;
+let op;
+let num2;
+
+let operate = (num1, op, num2) => {
+    if (op == "+"){
+        return add(num1, num2);
+    }else if(op == "-"){
+        return subtract(num1, num2);
+    }else if(op == "*"){
+        return multiply(num1, num2)
+    }else if(op == "/"){
+        return divide(num1,num2);
+    }
+}
