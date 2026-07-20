9ed8892c0da8a936e5527e04d1c8ed3310f1a158
Shafeek <shafeek.bakhach@gmail.com>
2026-07-20 06:21:52 -0700

    Adds keypress support, fixes +/- bug

--- script.js
+++ script.js
@@ -4,16 +4,24 @@ let multiply = (x1, x2) => x1 * x2;
 let divide = (x1, x2) => x1 / x2;
 
 let operate = (num1, op, num2) => {
-    if (op == "plus"){
+    if (op == "plus" || op == "+"){
         return add(num1, num2);
-    }else if(op == "minus"){
+    }else if(op == "minus" || op == "-"){
         return subtract(num1, num2);
-    }else if(op == "multiply"){
+    }else if(op == "multiply" || op == "*"){
         return multiply(num1, num2)
-    }else if(op == "divide"){
+    }else if(op == "divide" || op == "/"){
         return divide(num1,num2);
     }
 }
+
+let updateViewport = (num) => {viewport.textContent = num.toFixed(2)}
+let roundResult = (num) => {
+    // Source - https://stackoverflow.com/a/11832950
+    // Posted by Brian Ustas, modified by community. See post 'Timeline' for change history
+    // Retrieved 2026-07-20, License - CC BY-SA 4.0
+    return Math.round((num + Number.EPSILON) * 100) / 100
+};
 
 const calcButtons = document.querySelectorAll(".calc-button");
 const viewport = document.querySelector(".viewport p");
@@ -31,7 +39,6 @@ let reset = () => { // I noticed I kept repeating this logic with the AC and div
     op=undefined;
     result=0;
 }
-
 calcButtons.forEach((button) =>{
     button.addEventListener("click", (e) => {
         const classList = e.target.classList;
@@ -52,7 +59,7 @@ calcButtons.forEach((button) =>{
 
             if(num1 != 0 && num2 != 0){
                 result = operate(+num1, op, +num2);
-                viewport.textContent = result;
+                viewport.textContent = roundResult(result);
                 num1 = result;
                 num2 = '';
             }
@@ -63,7 +70,7 @@ calcButtons.forEach((button) =>{
 
             result = operate(+num1, op, +num2);
             updateSecond = false;
-            viewport.textContent = result;
+            viewport.textContent = roundResult(result);
         }
         if(e.target.id === 'ac'){
             viewport.textContent = '0';
@@ -81,7 +88,7 @@ calcButtons.forEach((button) =>{
         if(e.target.id === 'percent'){
             if (result != 0){
                 result /= 100;
-                viewport.textContent = +result;
+                viewport.textContent = roundResult(result);
             }
             else if(updateSecond){
                 num2 /= 100;
@@ -103,12 +110,89 @@ calcButtons.forEach((button) =>{
         }
         if(e.target.id === 'plus-minus'){
             if(updateSecond){
-                num2 = '-' + num2
-                viewport.textContent = num2;
+                let prefix = num2.slice(0,1);
+                if(prefix == '-'){
+                    num2 = num2.slice(1);
+                    viewport.textContent = num2;
+                }
+                else{
+                    num2 = '-' + num2
+                    viewport.textContent = num2;
+                }
             }else if(!updateSecond){
-                num1 = '-' + num1
-                viewport.textContent = num1;
+                let prefix = num1.slice(0,1);
+                if(prefix == '-'){
+                    num1 = num1.slice(1);
+                    viewport.textContent = num1;
+                }
+                else{
+                    num1 = '-' + num1
+                    viewport.textContent = num1;
+                }
+
             }
         }
     }
 )});
+
+document.addEventListener("keypress", (e) =>{
+    let key = e.key;
+    console.log(key);
+    let isNumber = ['0','1','2','3','4','5','6','7','8','9','0'].includes(key);
+    let isOperator = ['+', '-', '*', '/'].includes(key);
+
+    if(isNumber && updateSecond){
+        num2 += key;
+        viewport.textContent = num2;
+
+    }else if(isNumber && !updateSecond){
+        num1 += key;
+        viewport.textContent = num1;
+        }
+    if(isOperator){
+        if(num1 != 0 && num2 != 0){
+            result = operate(+num1, op, +num2);
+            
+            viewport.textContent = roundResult(result);
+            num1 = result;
+            num2 = '';
+        }
+        op = key;
+        updateSecond = true;
+    }
+    if(key == 'Enter' && num1 != 0 && num2 != 0){
+
+        result = operate(+num1, op, +num2);
+        updateSecond = false;
+        viewport.textContent = roundResult(result);
+    }
+
+    if(key == '%'){
+        if (result != 0){
+            result /= 100;
+            viewport.textContent = roundResult(result);
+        }
+        else if(updateSecond){
+            num2 /= 100;
+            viewport.textContent = +num2;
+        }else if(!updateSecond){
+            num1 /= 100;
+            viewport.textContent = +num1;
+        }
+    
+    }
+    if(key == "."){
+        if(updateSecond && !num2.includes('.')){
+            num2 += '.'
+            viewport.textContent = num2;
+        }else if(!updateSecond && !num1.includes('.')){
+            num1 += '.'
+            viewport.textContent = num1;
+        }
+    }
+    if(key == 'Enter' && result == Infinity){
+        viewport.textContent = 'You quackin fool!';
+        reset(); 
+    }
+}
+);
